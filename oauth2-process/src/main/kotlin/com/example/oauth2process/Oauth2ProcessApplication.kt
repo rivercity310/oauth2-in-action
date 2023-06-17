544db4c57030033bf5d96ac963a9039724fcf9c3
seungsu <h970126@gmail.com>
2023-06-17 22:29:16 +0900

    Inialize Project

---
+++ oauth2-process/src/main/kotlin/com/example/oauth2process/Oauth2ProcessApplication.kt
@@ -0,0 +1,11 @@
+package com.example.oauth2process
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Oauth2ProcessApplication
+
+fun main(args: Array<String>) {
+	runApplication<Oauth2ProcessApplication>(*args)
+}
---
+++ oauth2-process/src/test/kotlin/com/example/oauth2process/Oauth2ProcessApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oauth2process
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2ProcessApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
